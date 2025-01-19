d92169bd79c81bfe31214632a236c927968c06ac
SamLukeYes <samlukeyes123@gmail.com>
2025-01-19 20:09:06 +0800

    fail2ban: enable bantime-increment

--- services/fail2ban.nix
+++ services/fail2ban.nix
@@ -2,6 +2,11 @@
 
 {
   programs.rust-motd.settings.fail2_ban.jails = [ "sshd" ];
-  services.fail2ban.enable = true;
+
+  services.fail2ban = {
+    enable = true;
+    bantime-increment.enable = true;
+  };
+
   systemd.services.rust-motd.path = [ pkgs.fail2ban ];
 }
