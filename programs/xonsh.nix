4f7630ee9f3031e77ade80db2df008831691e4ee
SamLukeYes <samlukeyes123@gmail.com>
2025-10-02 22:30:33 +0800

    xonsh: add color style config

--- programs/xonsh.nix
+++ programs/xonsh.nix
@@ -1,6 +1,12 @@
 { config, ... }:
 
 {
-  programs.xonsh.enable = true;
+  programs.xonsh = {
+    enable = true;
+    config = ''
+      $XONSH_COLOR_STYLE = 'native'
+    '';
+  };
+
   users.defaultUserShell = config.programs.xonsh.package;
 }
