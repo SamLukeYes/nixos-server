559132c03f9e4bd8a6af2251d1a56aa15cf3ccaa
SamLukeYes <samlukeyes123@gmail.com>
2026-05-31 13:10:58 +0800

    bump nixos to 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     archix = {
       inputs.nixpkgs.follows = "nixpkgs";
