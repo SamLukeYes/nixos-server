7a1689bb1e5b7ae99a962fb06ba9be72a5ede7fd
SamLukeYes <samlukeyes123@gmail.com>
2026-06-19 23:50:00 +0800

    Add my cachix

--- programs/nix.nix
+++ programs/nix.nix
@@ -20,9 +20,11 @@
       flake-registry = "";
       narinfo-cache-negative-ttl = 300;
       substituters = [
+        "https://samlukeyes123.cachix.org"
         "https://cache.garnix.io"
       ];
       trusted-public-keys = [
+        "samlukeyes123.cachix.org-1:AsNJmUZGo73EYpBhz2w/XuysdNXxvf6WcZO7qWVh2TQ="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
